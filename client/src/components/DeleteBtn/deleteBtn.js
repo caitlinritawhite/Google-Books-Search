f081b1a449b9bbcbfc04b990e3d4cd76710f20a3
Caitlin White <caitlinwhite@Caitlins-MacBook-Air.local>
2020-11-30 14:47:53 -0500

    components

---
+++ client/src/components/DeleteBtn/deleteBtn.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./style.css"
+
+// The ...props means, spread all of the passed props onto this element
+// That way we don't have to define them all individually
+function DeleteBtn(props) {
+  return (
+    <button className="delete-btn btn" tabIndex="0" onClick={() => props.handleDeleteSubmit(props.id)} style={{ float: "right", marginBottom: 10 }}><i className="fas fa-trash"></i>
+      {props.children}
+    </button>
+  );
+}
+
+export default DeleteBtn;
